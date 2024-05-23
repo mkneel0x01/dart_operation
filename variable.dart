80248f3235171aa3ed9ebc0f279a11e985c742f3
mkneel0x01 <mkneel101@gmail.com>
2024-05-23 19:19:13 +0600

    complete variable part

---
+++ variable.dart
@@ -0,0 +1,25 @@
+void main() {
+/* String description;
+
+  void main() {
+    description = 'Feijoada!';
+    print(description);
+  } */
+
+  // variable
+
+  String first_name = "Mushfik";
+  String last_name = "Rahman";
+  String name = first_name + " " + last_name;
+
+  print(name);
+
+  // final/const variable
+
+  final pi = 3.1416;
+
+  // You can't change the value of a final/const variable:
+  // var pi = 1.254;
+
+  print(pi);
+}
