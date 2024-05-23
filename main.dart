8a60c6ad6abee568444f3e726ef01153fe2ea891
mkneel0x01 <mkneel101@gmail.com>
2024-05-23 19:04:42 +0600

    learn variable

--- main.dart
+++ main.dart
@@ -1,3 +1,16 @@
 void main() {
-  print("Welcome to Dart programming Language.");
+  // String description;
+
+  // void main() {
+  //   description = 'Feijoada!';
+  //   print(description);
+  // }
+
+  // variable
+
+  String first_name = "Mushfik";
+  String last_name = "Rahman";
+  String name = first_name + " " + last_name;
+
+  print(name);
 }
