7f61dcaa61cc8b34a00e033a1ee32429a6115523
mkneel0x01 <mkneel101@gmail.com>
2024-05-29 20:52:27 +0600

    data type complete

---
+++ data_type.dart
@@ -0,0 +1,47 @@
+void main() {
+  // Numbers
+// Strings
+// Booleans
+// Lists
+// Maps
+// Sets
+// Runes
+// Null
+
+  /* int num1 = 100;
+  double num2 = 12.25;
+  num num3 = 40;
+  num num4 = 52.2;
+
+  num result = num1 + num2 + num3 + num4;
+
+  print("Num 1 is $num1");
+  print("Num 2 is $num2");
+  print("Num 3 is $num3");
+  print("Num 4 is $num4");
+  print("Sum is $result");*/
+
+  // round the double value upto 2 decimal places
+
+  /*double price = 345.32555112;
+  print(price.toStringAsFixed(3));*/
+
+  /*String versityName = "Green University";
+  String address = "Rupgang";
+
+  print("University name is $versityName and location is $address");*/
+
+  // using \n and \t
+  // \n for new line
+  // \t for new tab
+  /*print("I am from \nUS. ");
+  print("I am from \tUS. ");*/
+
+// Set price value
+  /*num price = 10;
+  String withoutRawString = "The value of price is \t $price"; // regular String
+  String withRawString = r"The value of price is \t $price"; // raw String
+
+  print("Without Raw: $withoutRawString"); // regular result
+  print("With Raw: $withRawString"); // with raw result*/
+}
--- variable.dart
+++ variable.dart
@@ -8,12 +8,30 @@ void main() {
 
   // variable
 
-  String first_name = "Mushfik";
+  // here variable name contains value MK Neel.
+  //var name = "MK Neel";
+
+  // variable type
+
+// declaring variables
+  String name = "MK Neel";
+  String address = "USA";
+  num age = 20; // used to store any types of numbers
+  num height = 5.9;
+  bool isMarried = false;
+
+// printing variables value
+  print("Name is $name");
+  print("Address is $address");
+  print("Age is $age");
+  print("Height is $height");
+  print("Married Status is $isMarried");
+
+  /* String first_name = "Mushfik";
   String last_name = "Rahman";
   String name = first_name + " " + last_name;
 
-  print(name);
-
+  print(name);*/
   // final/const variable
 
   final pi = 3.1416;
